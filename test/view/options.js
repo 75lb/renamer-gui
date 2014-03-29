0c5c9f840f379d4784daa92c49744a425071833e
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-29 19:35:59 +0100

    tests ok

--- test/view/options.js
+++ test/view/options.js
@@ -3,7 +3,7 @@ var test = require("tape"),
     Results = renamer.Results,
     Options = require("../../view/Options");
    
-test("Options.get", function(t){
+test("options: set and get", function(t){
     var form = document.createElement("input");
     var options = new Options({
         find: document.createElement("input"),
@@ -12,18 +12,29 @@ test("Options.get", function(t){
         insensitive: document.createElement("input"),
         dryRun: document.createElement("input"),
     });
-    options.find.value = "find";
-    options.replace.value = "replace";
-    options.regex.checked = "true";
-    options.insensitive.checked = "true";
-    options.dryRun.checked = "true";
+    options.find = "find";
+    options.replace = "replace";
+    options.regex = true;
+    options.insensitive = true;
+    options.dryRun = true;
 
-    t.deepEqual(options.collect(), {
-        find: "find",
-        replace: "replace",
-        regex: "true",
-        insensitive: "true",
-        dryRun: "true"
-    });
+    t.equal(options._el.find.value, "find", "set find");
+    t.equal(options._el.replace.value, "replace", "set replace");
+    t.equal(options._el.regex.checked, true, "set regex");
+    t.equal(options._el.insensitive.checked, true, "set insensitive");
+    t.equal(options._el.dryRun.checked, true, "set dryRun");
+    
+    options._el.find.value = "find2";
+    options._el.replace.value = "replace2";
+    options._el.regex.checked = false;
+    options._el.insensitive.checked = false;
+    options._el.dryRun.checked = false;
+
+    t.equal(options.find, "find2");
+    t.equal(options.replace, "replace2");
+    t.equal(options.regex, false);
+    t.equal(options.insensitive, false);
+    t.equal(options.dryRun, false);
+    
     t.end();
 });
--- view/Options.js
+++ view/Options.js
@@ -4,6 +4,52 @@ var w = require("wodge"),
 
 module.exports = Options;
 
-function Options(options){
-    
+function Options(elements){
+    var self = this;
+    this._el = elements;
+    Object.defineProperty(this, "find", {
+        enumerable: true,
+        set: function setFind(val){
+            self._el.find.value = val;
+        },
+        get: function getFind(){
+            return self._el.find.value;
+        }
+    });
+    Object.defineProperty(this, "replace", {
+        enumerable: true,
+        set: function setReplace(val){
+            self._el.replace.value = val;
+        },
+        get: function getReplace(){
+            return self._el.replace.value;
+        }
+    });
+    Object.defineProperty(this, "regex", {
+        enumerable: true,
+        set: function setRegex(val){
+            self._el.regex.checked = val;
+        },
+        get: function getRegex(){
+            return self._el.regex.checked;
+        }
+    });
+    Object.defineProperty(this, "insensitive", {
+        enumerable: true,
+        set: function setInsensitive(val){
+            self._el.insensitive.checked = val;
+        },
+        get: function getInsensitive(){
+            return self._el.insensitive.checked;
+        }
+    });
+    Object.defineProperty(this, "dryRun", {
+        enumerable: true,
+        set: function setDryRun(val){
+            self._el.dryRun.checked = val;
+        },
+        get: function getDryRun(){
+            return self._el.dryRun.checked;
+        }
+    });
 }
